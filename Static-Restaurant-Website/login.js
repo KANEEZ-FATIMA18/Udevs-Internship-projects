cd9e759112ffbaa32d9e5af61c5435f5810bfb0d
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2025-10-29 02:24:59 +0500

    changes

--- Static-Restaurant-Website/login.js
+++ Static-Restaurant-Website/login.js
@@ -1,11 +1,27 @@
     const loginForm = document.getElementById("loginForm");
         const loginCard = document.getElementById("loginCard");
         const tableMain = document.querySelector(".tableMain");
+        const usernameInput = document.getElementById("Username");
+        const passwordInput = document.getElementById("password");
 
         loginForm.addEventListener("submit", (e) => {
             e.preventDefault();
-            loginCard.style.display = "none";
+            const expectedUsername = "staff_members";
+            const expectedPassword = "restaurant2024";
+
+            if (usernameInput && passwordInput) {
+                const enteredUsername = usernameInput.value;
+                const enteredPassword = passwordInput.value;
+
+                if (enteredUsername === expectedUsername && enteredPassword === expectedPassword) {
+                      loginCard.style.display = "none";
             tableMain.style.display = "block";
-            tableMain.scrollIntoView({ behavior: "smooth" });
+                } else {
+                    alert("Invalid username or password. Please try again.");
+                }
+
+               tableMain.scrollIntoView({ behavior: "smooth" });
             loginForm.reset();
+            }
+      
         });
